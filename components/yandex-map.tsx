ae36e5fd384819fb7424274bd718a9d65378fc0d
Andreyleluk-GAS <andreyleluk@gmail.com>
2026-03-17 13:24:08 +0500

    Update Yandex Map coordinates in iframe source

--- components/yandex-map.tsx
+++ components/yandex-map.tsx
@@ -1,10 +1,14 @@
 "use client"
 
+// Координаты: г. Екатеринбург, ул. Шефская, 3АВ
+// Чтобы изменить координаты карты, обновите значения в URL ниже:
+// ll=ДОЛГОТА,ШИРОТА и pt=ДОЛГОТА,ШИРОТА (формат Яндекс Карт)
+// Текущие координаты: 60.6595 (долгота), 56.8725 (широта)
 export function YandexMap() {
   return (
     <div className="w-full rounded-xl overflow-hidden border border-border shadow-sm">
       <iframe
-        src="https://yandex.ru/map-widget/v1/?apikey=b658001c-0f51-4686-a883-741cf3c7b16f&ll=60.8950%2C56.8801&z=16&pt=60.8950%2C56.8801%2Cpm2rdm&text=%D0%95%D0%BA%D0%B0%D1%82%D0%B5%D1%80%D0%B8%D0%BD%D0%B1%D1%83%D1%80%D0%B3%2C+%D1%83%D0%BB.+%D0%A8%D0%B5%D1%84%D1%81%D0%BA%D0%B0%D1%8F%2C+3%D0%90%D0%92"
+        src="https://yandex.ru/map-widget/v1/?apikey=b658001c-0f51-4686-a883-741cf3c7b16f&ll=60.6595%2C56.8725&z=16&pt=60.6595%2C56.8725%2Cpm2rdm&text=%D0%95%D0%BA%D0%B0%D1%82%D0%B5%D1%80%D0%B8%D0%BD%D0%B1%D1%83%D1%80%D0%B3%2C+%D1%83%D0%BB.+%D0%A8%D0%B5%D1%84%D1%81%D0%BA%D0%B0%D1%8F%2C+3%D0%90%D0%92"
         width="100%"
         height="380"
         frameBorder="0"
